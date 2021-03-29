1078ad5523adee1895b818d5c21885ce6d61065f
prokopenko7797 <prokopenko7797v@gmail.com>
2021-03-29 16:07:11 +0300

    Add DB

---
+++ GPSNote/GPSNote/GPSNote/Constants/Constant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSNote.Constants
+{
+    public static class Constant
+    {
+        public const string DB_Name = "sqlite.db";
+        public const int MinLoginLength = 4;
+        public const int MaxLoginLength = 16;
+        public const int MinPasswordLength = 8;
+        public const int MaxPasswordLength = 16;
+        public const int NonAuthorized = -1;
+        public const int SQLError = -1;
+        public const string Login = "Login";
+
+    }
+}
---
+++ GPSNote/GPSNote/GPSNote/Models/IEntityModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSNote.Models
+{
+    public interface IEntityModel
+    {
+        int id { get; set; }
+    }
+}
