8b1daa5dba476ae8eb24e4dbdbe60b8c272055fe
prokopenko7797 <prokopenko7797v@gmail.com>
2021-04-27 18:21:38 +0300

    fonts

--- GPSNote/GPSNote/GPSNote/Properties/AssemblyInfo.cs
+++ GPSNote/GPSNote/GPSNote/Properties/AssemblyInfo.cs
@@ -1,3 +1,23 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Montserrat-Black.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-BlackItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-BoldItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-ExtraBold.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-ExtraBoldItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-ExtraLight.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-ExtraLightItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-Italic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-LightItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-MediumItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-SemiBoldItalic.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-Thin.ttf", Alias = "Monserat")]
+[assembly: ExportFont("Montserrat-ThinItalic.ttf", Alias = "Monserat")]
